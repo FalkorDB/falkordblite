1de3e257b5013d66eb317eaae18f3e4c0dda27f0
Guy Korland <gkorland@gmail.com>
2026-02-04 22:52:09 +0200

    Change Redis download URL to GitHub
    Updated Redis download URL to use GitHub repository.

--- build_scripts/update_redis_server.py
+++ build_scripts/update_redis_server.py
@@ -9,7 +9,7 @@ import tempfile
 
 
 redis_version = os.environ.get('REDIS_VERSION', '8.2.3')
-url = f'https://download.redis.io/releases/redis-{redis_version}.tar.gz'
+url = f'https://github.com/redis/redis/archive/refs/tags/{redis_version}.tar.gz'
 
 
 if __name__ == "__main__":
